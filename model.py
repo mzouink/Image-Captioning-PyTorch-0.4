0027731abfaef29d17716366cc974a911823f7bd
mzouink <zouinkhi.marwan@gmail.com>
2019-06-13 16:59:05 +0000

    work on training

--- model.py
+++ model.py
@@ -22,12 +22,38 @@ class EncoderCNN(nn.Module):
     
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+    def __init__(self, embed_size, hidden_size, vocab_size,  n_layers=1):
+        super().__init__()
+        
+        ## TODO: define the LSTM
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, n_layers, batch_first=True)
+        
+        ## TODO: define the final, fully-connected output layer
+        self.fc = nn.Linear(hidden_size, vocab_size)
+        
+        self.hidden = (torch.zeros(1,1,hidden_size),torch.zeros(1,1,hidden_size))
     
     def forward(self, features, captions):
-        pass
+        captions = self.embed(captions[:,:-1])
+        
+        features = features.unsqueeze(1)
+        
+        inputs = torch.cat((features,captions),1)
+        
+        lstm_output,_ = self.lstm(inputs,None)
+        
+        outputs = self.fc(lstm_output)
+
+        return  outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        result = []
+        for i in range(max_len):
+            outputs, hidden = self.lstm(inputs, states)
+            outputs = self.fc(outputs.squeeze(1))
+            target_index = outputs.max(1)[1]
+            result.append(target_index.item())
+            inputs = self.embed(target_index).unsqueeze(1)
+        return result
